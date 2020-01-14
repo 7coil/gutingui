030146c4f0009242b3d8fa11a4ba8a51fe4253b8
Leondro Lio <leondrolio@gmail.com>
2020-01-14 15:48:00 +0000

    Support `rgb(x,y,x)` colours

--- gutingui/constants.py
+++ gutingui/constants.py
@@ -3,6 +3,7 @@
 # See the file 'COPYING' for copying permission.
 
 from __future__ import division
+import re
 
 from string import hexdigits
 
@@ -24,12 +25,22 @@ ColorMap = {'aqua': '#00ffff',
             'white': '#ffffff',
             'yellow': '#ffff00'}
 
+rgb_pattern = r'rgb\(\s*(\d|\d\d|1\d\d|2[0-4]\d|25[0-6])\s*,\s*(\d|\d\d|1\d\d|2[0-4]\d|25[0-6])\s*,\s*(\d|\d\d|1\d\d|2[0-4]\d|25[0-6])\)'
 
 def map_color(color):
     if color is None:
         return None
     elif color.lower() in ColorMap:
         return ColorMap[color.lower()]
-    elif len(color) and all(c in hexdigits for c in color):
+    elif isinstance(color, str):
+      rgb_match = re.match(rgb_pattern, color)
+
+      if len(color) and all(c in hexdigits for c in color):
         return '#%s' % color
+      elif rgb_match:
+        r = rgb_match.group(1)
+        g = rgb_match.group(2)
+        b = rgb_match.group(3)
+        return '#%02x%02x%02x' % (int(r), int(g), int(b))
+
     return color
